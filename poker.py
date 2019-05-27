4950acc0e7927fe2cbd40db003ffedef7a1c6f7d
Tr4shL0rd <ketil04@gmail.com>
2019-05-27 10:47:37 +0200

    math is hard

--- poker.py
+++ poker.py
@@ -14,37 +14,46 @@ rules()
 
 print()
 def bets():
+        bets.yourDebt = 0
         bets.yourBet = input("How many do you want to bet? " )
-        confirmation = input("you bet {} chips, are you sure? ".format(bets.yourBet))
         bets.yourBet = int(bets.yourBet)
+        if bets.yourBet > 120: 
+                print("you dont have {} chips".format(bets.yourBet))
+                bets()
+        elif bets.yourBet < 1:
+                print("you have to bet something")
+                bets()
+        confirmation = input("you bet {} chips, are you sure? ".format(bets.yourBet)).lower()
         if confirmation == "no":
                 bets()
-
 bets()
 print(clear)
-
+print()
 def game():
         print()
         #stupid shit that has all the cards and bets
-        randomCard1 = random.choice(["Rock", "Paper", "Scissors","Rock", "Scissors", "Rock", "Paper", "Scissors","Rock", "Scissors" ])
-        randomCard2 = random.choice(["Rock", "Paper", "Scissors","Rock", "Scissors", "Rock", "Paper", "Scissors","Rock", "Scissors" ])
-        randomCard3 = random.choice(["Rock", "Paper", "Scissors","Rock", "Scissors", "Rock", "Paper", "Scissors","Rock", "Scissors" ])
+        randomCard1 = random.choice(["rock", "paper", "scissors","rock", "scissors", "rock", "paper", "scissors","rock", "scissors" ])
+        randomCard2 = random.choice(["rock", "paper", "scissors","rock", "scissors", "rock", "paper", "scissors","rock", "scissors" ])
+        randomCard3 = random.choice(["rock", "paper", "scissors","rock", "scissors", "rock", "paper", "scissors","rock", "scissors" ])
 
-        CPUrandomCard2 = random.choice(["Rock", "Paper", "Scissors","Rock", "Scissors", "Rock", "Paper", "Scissors","Rock", "Scissors" ])
-        CPUrandomCard3 = random.choice(["Rock", "Paper", "Scissors","Rock", "Scissors", "Rock", "Paper", "Scissors","Rock", "Scissors" ])
-        CPUrandomCard1 = random.choice(["Rock", "Paper", "Scissors","Rock", "Scissors", "Rock", "Paper", "Scissors","Rock", "Scissors" ])
+        CPUrandomCard2 = random.choice(["rock", "paper", "scissors","rock", "scissors", "rock", "paper", "scissors","rock", "scissors" ])
+        CPUrandomCard3 = random.choice(["rock", "paper", "scissors","rock", "scissors", "rock", "paper", "scissors","rock", "scissors" ])
+        CPUrandomCard1 = random.choice(["rock", "paper", "scissors","rock", "scissors", "rock", "paper", "scissors","rock", "scissors" ])
         CPUbet = random.randrange(1, 120)
 
         #some stupid shit that makes the game work somehow
         cpuCardArray = [CPUrandomCard1, CPUrandomCard2, CPUrandomCard3]
         cpuCard = random.choice(cpuCardArray)
-
+        print(bets.yourBet)
         print(colored("let the game begin", "green"))
         print()
         
         yourHand = print ("you got {}, {}, {}".format(randomCard1,randomCard2,randomCard3))
         yourChoices = [randomCard1, randomCard2, randomCard3]
-        yourCard = input("what do you want to draw? ")
+        yourCard = input("what do you want to draw? ").lower()
+        if yourCard != yourChoices:
+                print(colored("thats not a valid card!", "red"))
+                game()
         print("we will draw on 3")
         print()
         print("1")
@@ -68,26 +77,27 @@ def game():
                 else:
                         exit()
 
-        elif yourCard == "Rock" and cpuCard == "Paper":
+        elif yourCard == "rock" and cpuCard == "paper":
                 print("you chose {}".format(yourCard))
                 print("your opponent chose {}".format(cpuCard))
                 print(colored("you LOST!", "red"))
                 print("you lost {} chips, and you opponent gains {} chips".format(bets.yourBet, CPUbet))
+                bets.yourDebt = bets.yourDebt + bets.yourBet * 1000
                 bets.yourBet =  120 - bets.yourBet
-                print("your owe {} thousand usd".format(bets.yourBet))
+                print("your owe {} usd".format(bets.yourDebt))
                 retry = input("play again? ")
                 if retry == "yes":
                         game()
                 else:
                         exit()
 
-        elif yourCard == "Rock" and cpuCard == "Scissors":
+        elif yourCard == "rock" and cpuCard == "scissors":
                 print("you chose {}".format(yourCard))
                 print("your opponent chose {}".format(cpuCard))
                 print(colored("you WIN!", "green"))
                 print("you gained {} chips, and you opponent lost {} chips".format(bets.yourBet, CPUbet))
-                bets.yourBet =  bets.yourBet + CPUbet
-                print("you won {} thousand usd".format(bets.yourBet))
+                bets.yourBet =  bets.yourBet + CPUbet * 1000
+                print("you won {} usd".format(bets.yourBet))
                 retry = input("play again? ")
                 if retry == "yes":
                         game()
@@ -95,26 +105,27 @@ def game():
                         exit()
 
 
-        elif yourCard == "Paper" and cpuCard == "Scissors":
+        elif yourCard == "paper" and cpuCard == "scissors":
                 print("you chose {}".format(yourCard))
                 print("you opponent chose {}".format(cpuCard))
                 print(colored("you LOST!", "red"))
                 print("you lost {} chips, and you opponent gains {} chips".format(bets.yourBet, CPUbet))
+                bets.yourDebt = bets.yourDebt + bets.yourBet * 1000
                 bets.yourBet =  120 - bets.yourBet
-                print("you owe {} thousand usd".format(bets.yourBet))
+                print("your owe {} usd".format(bets.yourDebt))
                 retry = input("play again? ")
                 if retry == "yes":
                         game()
                 else:
                         exit()
 
-        elif yourCard == "Paper" and cpuCard == "Rock":
+        elif yourCard == "paper" and cpuCard == "rock":
                 print("you chose {}".format(yourCard))
                 print("you opponent chose {}".format(cpuCard))
                 print(colored("you WIN!", "green"))
                 print("you gained {} chips, and you opponent lost {} chips".format(bets.yourBet, CPUbet))
-                bets.yourBet =  bets.yourBet + CPUbet
-                print("you won {} thousand usd ".format(bets.yourBet))
+                bets.yourBet =  bets.yourBet + CPUbet * 1000
+                print("you won {}  usd ".format(bets.yourBet))
                 retry = input("play again? ")
                 if retry == "yes":
                         game()
@@ -122,25 +133,26 @@ def game():
                         exit()
 
                 
-        elif yourCard == "Scissor" and cpuCard == "Rock":
+        elif yourCard == "scissors" and cpuCard == "rock":
                 print("you chose {}".format(yourCard))
                 print("your opponent chose {}".format(cpuCard))
                 print(colored("you LOST!", "red"))
                 print("you lost {} chips, and you opponent gains {} chips".format(bets.yourBet, CPUbet))
+                bets.yourDebt = bets.yourDebt + bets.yourBet * 1000
                 bets.yourBet =  120 - bets.yourBet
-                print("you owe {} thousand usd".format(bets.yourBet))
+                print("your owe {} usd".format(bets.yourDebt))
                 retry = input("play again? ")
                 if retry == "yes":
                         game()
                 else:
                         exit()
                         
-        elif yourCard == "Scissors" and cpuCard == "paper":
+        elif yourCard == "scissors" and cpuCard == "paper":
                 print("you chose {}".format(yourCard))
                 print("you opponent chose {}".format(cpuCard))
                 print(colored("you WIN!", "green"))
                 print("you gained {} chips, and you opponent lost {} chips".format(bets.yourBet, CPUbet))
-                bets.yourBet =  120 - bets.yourBet
+                bets.yourBet =  120 - bets.yourBet * 1000
                 print("you won {}".format(bets.yourBet))
                 retry = input("play again? ")
                 if retry == "yes":
